92aca014fb3a7c3d61b94a243ae96ab5167b90d0
Bekaensarmu <beekanensarmu@gmail.com>
2023-09-25 14:28:17 -0700

    3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -2,8 +2,8 @@
 
 void swap_ints(int *bek, int *len);
 int lomuto_partition(int *len_array, size_t len_size, int len_left, int len_right);
-void lomuto_sort(int *array, size_t size, int left, int right);
-void quick_sort(int *array, size_t size);
+void lomuto_sort(int *len_array, size_t len_size, int len_left, int len_right);
+void quick_sort(int *len_array, size_t len_size);
 
 /**
  * swap_ints - Swap two integers in an array.
@@ -58,38 +58,38 @@ int lomuto_partition(int *len_array, size_t len_size, int len_left, int len_righ
 
 /**
  * lomuto_sort - Implement the quicksort algorithm through recursion.
- * @array: An array of integers to sort.
- * @size: The size of the array.
- * @left: The starting index of the array partition to order.
- * @right: The ending index of the array partition to order.
+ * @len_array: An array of integers to sort.
+ * @len_size: The size of the array.
+ * @len_left: The starting index of the array partition to order.
+ * @len_right: The ending index of the array partition to order.
  *
  * Description: Uses the Lomuto partition scheme.
  */
-void lomuto_sort(int *array, size_t size, int left, int right)
+void lomuto_sort(int *len_array, size_t len_size, int len_left, int len_right)
 {
 	int part;
 
-	if (right - left > 0)
+	if (len_right - len_left > 0)
 	{
-		part = lomuto_partition(array, size, left, right);
-		lomuto_sort(array, size, left, part - 1);
-		lomuto_sort(array, size, part + 1, right);
+		part = lomuto_partition(len_array, len_size, len_left, len_right);
+		lomuto_sort(len_array, len_size, len_left, part - 1);
+		lomuto_sort(len_array, len_size, part + 1, len_right);
 	}
 }
 
 /**
  * quick_sort - Sort an array of integers in ascending
  *              order using the quicksort algorithm.
- * @array: An array of integers.
- * @size: The size of the array.
+ * @len_array: An array of integers.
+ * @len_size: The size of the array.
  *
  * Description: Uses the Lomuto partition scheme. Prints
  *              the array after each swap of two elements.
  */
-void quick_sort(int *array, size_t size)
+void quick_sort(int *len_array, size_t len_size)
 {
-	if (array == NULL || size < 2)
+	if (len_array == NULL || len_size < 2)
 		return;
 
-	lomuto_sort(array, size, 0, size - 1);
+	lomuto_sort(len_array, len_size, 0, len_size - 1);
 }
